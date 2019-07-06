73a9ca4face0be5cc66e85b682c26b2aa2e8c9b1
Muhammad Khoirul Anam <mkhoir.anam@gmail.com>
2019-07-07 05:22:24 +0700

    add logout button

--- application/views/auth/index.php
+++ application/views/auth/index.php
@@ -28,4 +28,4 @@
 	<?php endforeach;?>
 </table>
 
-<p><?php echo anchor('auth/create_user', lang('index_create_user_link'))?> | <?php echo anchor('auth/create_group', lang('index_create_group_link'))?></p>
+<p><?php echo anchor('auth/create_user', lang('index_create_user_link'))?> | <?php echo anchor('auth/create_group', lang('index_create_group_link'))?> | <?php echo anchor('auth/logout', lang('logout'))?></p>
